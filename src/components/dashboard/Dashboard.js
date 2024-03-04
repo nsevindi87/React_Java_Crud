3ff0c5bf472ea5611258e40ba42b9428bfc1bf0f
Nizami Sevindi <nsevindi87@gmail.com>
2024-03-04 11:47:45 +0100

    fetched users

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Container } from '@mui/system'
+import React, { useEffect,useState } from 'react'
 
 const Dashboard = () => {
+
+  const [users,setUsers]=useState([])
+
+  const fetchUsers = async ()=>{
+    try {
+      const response = await fetch("http://localhost:8080/api/users")
+      const result = await response.json();
+      setUsers(result)
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(()=>{
+    fetchUsers()
+  },[])
+
   return (
     <>
-    <h1>Dashboard</h1>
+      <Container className="mt-4">
+        
+      </Container>
 
     </>
   )
